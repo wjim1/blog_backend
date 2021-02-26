ea1d731f2df053ba1b240dd3516989393158af88
wj <969576137@qq.com>
2021-02-26 17:36:03 +0800

    return resource

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,22 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Constants\PublicCode;
+use App\Http\Resources\ArticleResource;
 use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    public function index(Request $request): \Illuminate\Http\JsonResponse
+    public function index(Request $request): \Illuminate\Http\Resources\Json\AnonymousResourceCollection
     {
         $category_id = $request->get('category');
         $titles = Article::query()
             ->where('category_id', $category_id)
-            ->get();
-        return response()->json([
-            'code' => PublicCode::SUCCESS_CODE,
-            'msg' => PublicCode::SUCCESS_MSG,
-            'data' =>  $titles,
-            'res' => $category_id
-        ]);
+            ->paginate(10);
+        return ArticleResource::collection($titles);
+//        return response()->json([
+//            'code' => PublicCode::SUCCESS_CODE,
+//            'msg' => PublicCode::SUCCESS_MSG,
+//            'data' =>  $titles
+//        ]);
     }
 }
